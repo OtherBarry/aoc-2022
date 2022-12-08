424f289200c31f57e0d7d83aff09aa1445a993e0
Charlie Wilson <charlie@spiralblue.space>
2022-12-08 12:40:26 +1100

    Day 3

---
+++ day-3/main.py
@@ -0,0 +1,85 @@
+from typing import Any
+
+PRIORITY_MAP = {
+    "a": 1,
+    "b": 2,
+    "c": 3,
+    "d": 4,
+    "e": 5,
+    "f": 6,
+    "g": 7,
+    "h": 8,
+    "i": 9,
+    "j": 10,
+    "k": 11,
+    "l": 12,
+    "m": 13,
+    "n": 14,
+    "o": 15,
+    "p": 16,
+    "q": 17,
+    "r": 18,
+    "s": 19,
+    "t": 20,
+    "u": 21,
+    "v": 22,
+    "w": 23,
+    "x": 24,
+    "y": 25,
+    "z": 26,
+    "A": 27,
+    "B": 28,
+    "C": 29,
+    "D": 30,
+    "E": 31,
+    "F": 32,
+    "G": 33,
+    "H": 34,
+    "I": 35,
+    "J": 36,
+    "K": 37,
+    "L": 38,
+    "M": 39,
+    "N": 40,
+    "O": 41,
+    "P": 42,
+    "Q": 43,
+    "R": 44,
+    "S": 45,
+    "T": 46,
+    "U": 47,
+    "V": 48,
+    "W": 49,
+    "X": 50,
+    "Y": 51,
+    "Z": 52,
+}
+
+
+def chunk_list(list_: list[Any], chunk_size: int) -> list[list[Any]]:
+    return [list_[i : i + chunk_size] for i in range(0, len(list_), chunk_size)]
+
+
+def part_1(items: list[list[int]]) -> int:
+    result = 0
+    for pack in items:
+        midpoint = len(pack) // 2
+        intersection = set(pack[:midpoint]) & set(pack[midpoint:])
+        result += intersection.pop()
+    return result
+
+
+def part_2(items: list[list[int]]) -> int:
+    result = 0
+    for chunk in chunk_list(items, 3):
+        a, b, c = chunk
+        intersection = set(a) & set(b) & set(c)
+        result += intersection.pop()
+    return result
+
+
+with open("day-3/input.txt") as f:
+    input = f.read().splitlines()
+    packs = [[PRIORITY_MAP[c] for c in line] for line in input]
+    print(f"Part 1: {part_1(packs)}")
+    print(f"Part 2: {part_2(packs)}")
