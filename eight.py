c2512c8851fe374d1cdd92ece80b0e0ae53cd91d
Charlie Wilson <charlie.wilson4@gmail.com>
2022-12-09 18:39:16 +1100

    Day 8 Part 2

--- eight.py
+++ eight.py
@@ -1,6 +1,7 @@
+import numpy as np
+
 from base import Problem
 
-import numpy as np
 
 def test_array_visibility(array: np.ndarray, mask: np.ndarray) -> None:
     current_max = -1
@@ -13,6 +14,25 @@ def test_array_visibility(array: np.ndarray, mask: np.ndarray) -> None:
         if v > current_max:
             current_max = v
             mask[i] = True
+
+
+def determine_visibility(array: np.ndarray, coordinates: tuple[int, int]) -> int:
+    results = []
+    value = array[coordinates]
+    for direction in ((1, 0), (-1, 0), (0, 1), (0, -1)):
+        visibility = 0
+        next_coordinates = coordinates
+        while True:
+            next_coordinates = tuple(np.add(next_coordinates, direction))
+            if not all(0 <= c < 99 for c in next_coordinates):
+                break
+            visibility += 1
+            if array[next_coordinates] >= value:
+                break
+        if visibility == 0:
+            return 0
+        results.append(visibility)
+    return np.product(results)
 
 
 class Problem8(Problem):
@@ -33,4 +53,7 @@ class Problem8(Problem):
         return np.sum(binary_map)
 
     def part_2(self) -> int:
-        return "TBD"
+        visibility_map = np.zeros(self.grid.shape, dtype=np.uint32)
+        for (x, y), element in np.ndenumerate(self.grid):
+            visibility_map[x, y] = determine_visibility(self.grid, (x, y))
+        return np.max(visibility_map)
